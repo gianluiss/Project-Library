06b3f8ced04c1f64a20aee95f76c08a3e2b97aca
luisito0028 <gianluistapia28@gmail.com>
2026-01-31 02:46:32 +0800

    Added script and styles for heading & dialog, - opening and closing dialog box script - assets for heading and font

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
+const addBookBtn = document.querySelector(".add-book-btn");
+const dialog = document.querySelector("dialog");
+const closeBtn = document.querySelector(".close-btn");
+
 const myLibrary = [];
 
-function Book() {
+function Book(id, title, author, genre, isRead) {
     // the constructor...
     if(!new.target) {
         throw Error("Use 'new' keyword to call constructor");
     }
 
-    this.id = crypto.randomUUID();
+    this.id = id;
+    this.title = title,
+    this.author = author;
+    this.genre = genre;
+    this.isRead = isRead;
 }
 
 /*
 author, title, number of pages, whether it’s been read and anything else you might want.
 */
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, genre, isRead) {
     // take params, create a book then store it in the array
+    myLibrary.push(new Book(crypto.randomUUID(), title, author, genre, isRead));
 }
 
-/*
-const book1 = new Book();
-console.log(book1.id);
+addBookToLibrary("Harry Potta", "J.K. Rowling", "Fantasy", false);
+addBookToLibrary("Jujutsu Kaisen", "Gege Akutami", "Action", true);
+console.log(myLibrary);
 
-const book2 = new Book();
-console.log(book2.id);
-*/
+function displayBook() {
+    myLibrary.forEach( (book) => {
+        console.log(`ID: ${book.id}\nTitle: ${book.title}\nAuthor: ${book.author}\nGenre: ${book.genre}\nisRead: ${book.isRead}\n==========================`);
+    });
+}
+
+displayBook();
+
+addBookBtn.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+closeBtn.addEventListener("click", () => {
+    dialog.close();
+});
